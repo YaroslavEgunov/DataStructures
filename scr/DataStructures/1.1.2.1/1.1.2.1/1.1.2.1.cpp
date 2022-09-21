5580d1692b7b48c74713314abdd7bb3cea48c6b4
YaroslavEgunov517-1 <99376130+YaroslavEgunov517-1@users.noreply.github.com>
2022-09-21 23:32:52 +0700

    Сделано 1.1.2.1

---
+++ scr/DataStructures/1.1.2.1/1.1.2.1/1.1.2.1.cpp
@@ -0,0 +1,38 @@
+﻿// 1.1.2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	int array[10];
+	srand(time(0));
+	cout << "Source array is: ";
+
+	for (int i=0; i<10; i++)
+	{
+		array[i] =  1 + rand() % 200 + (-100);
+		cout << array[i] << " ";
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i+1; j < 10; j++)
+		{
+			if (array[i] > array[j])
+			{
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+
+	cout << "\nSorted array is: ";
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
