57e68917a605fafc36113d2e135763879efe8804
YaroslavEgunov517-1 <99376130+YaroslavEgunov517-1@users.noreply.github.com>
2022-09-25 17:02:06 +0700

    Сделаны все с 1.1.2.3 по 1.1.3.3

---
+++ scr/DataStructures/1.1.2.3/1.1.2.3/1.1.2.3.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char array[8];
+    cout << "Enter array of 8 chars" << endl;
+    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+    {
+        cout << "a[" << i << "]: ";
+        cin >> array[i];
+    }
+    cout << "Your array is:" << endl;
+    for (char symbol : array)
+    {
+        cout << symbol << " ";
+    }
+    cout << endl << "All letters in your array:" << endl;
+    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+    {
+        if ((97 <= array[i] && array[i] <= 122) || (65 <= array[i] && array[i] <= 90))
+        {
+            cout << array[i] << " ";
+        }
+    }
+}
---
+++ scr/DataStructures/1.1.3.1/1.1.3.1/1.1.3.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double GetPower(double base, int exponent)
+{
+    cout << base << " ^ " << exponent << " = ";
+    return pow(base, exponent);
+}
+
+void DemoGetPower(double base, int exponent)
+{
+    cout << GetPower(base, exponent) << endl;
+}
+
+int main()
+{
+    cout << GetPower(3, 7) << endl;
+    cout << GetPower(-6.2, 4) << endl;
+    cout << GetPower(1.7, 12) << endl;
+    DemoGetPower(5, 5);
+    DemoGetPower(-3, 3);
+    DemoGetPower(999999, 0);
+}
